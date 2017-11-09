5838f5a08eaaf92e1fd383ba10ab48fdef60b0d2
Mikael Hadler <mikaelhadler@gmail.com>
2017-11-09 19:17:09 -0200

    alter default port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen( process.env.PORT || 8081, function(){
-    console.log('Servidor rodando na porta 8080.');
+    console.log('Servidor rodando na porta 8081.');
 });
