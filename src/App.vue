2f79cea497c7e42ed4e635e86ea05a95b8a58096
Mikael Hadler <mikaelhadler@gmail.com>
2018-03-05 21:45:32 -0300

    Subindo estrutura ongbook vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  name: 'App'
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        key: ''
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
